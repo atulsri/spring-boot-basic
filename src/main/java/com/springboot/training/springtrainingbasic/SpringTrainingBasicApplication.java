c4701081682e0a6ef850b67a211e05cdc0bf95f8
SRIVATU <SRIVATU@hilti.com>
2018-08-27 17:48:05 +0530

    Initial commit

---
+++ src/main/java/com/springboot/training/springtrainingbasic/SpringTrainingBasicApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.training.springtrainingbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainingBasicApplication.class, args);
+	}
+}
